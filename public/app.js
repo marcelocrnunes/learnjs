2b3ba9064dd314082d6798336865eab43352c530
Marcelo Nunes <m@mcrn.com.br>
2016-12-22 21:03:56 -0200

    [fix] Subscribes to the hash change event

--- public/app.js
+++ public/app.js
@@ -18,11 +18,8 @@ learnjs.showView = function(hash) {
 }
 
 learnjs.appOnReady = function() {
-	learnjs.appOnReady = function() {
-		window.onhashchange = function() {
-			learnjs.showView(window.location.hash);
-		};
-	}
-
+	window.onhashchange = function() {
+		learnjs.showView(window.location.hash);
+	};
 	learnjs.showView(window.location.hash);
 }
